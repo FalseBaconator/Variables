8ae25620775ce56126011467ef8a855ab8400993
FalseBaconator <113189775+FalseBaconator@users.noreply.github.com>
2022-09-20 09:32:11 -0300

    Added the three main types of variables with examples and displays

--- Variables/Program.cs
+++ Variables/Program.cs
@@ -10,6 +10,36 @@ namespace Variables
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Variables");
+            Console.WriteLine();
+
+            //Variables
+            //Integers
+            int score; //Declared
+            score = 0; //Initialization
+            Console.WriteLine("Score: " + score); //Display
+            score = 1000000; //Change
+            Console.WriteLine("Score: " + score); //Display
+            Console.WriteLine();
+
+            //Floating Points (decimals)
+            float lapTime; //Declare
+            lapTime = 0.0f; //Initial
+            Console.WriteLine("Time: " + lapTime); //Display
+            lapTime = 65.357f; //Change
+            Console.WriteLine("Time: " + lapTime); //Display
+            Console.WriteLine();
+
+            //Strings of text
+            string title; //Declared
+            title = "Rookie"; //Initial
+            Console.WriteLine("Title: " + title); //Display
+            title = "Slightly Skilled Rookie"; //Change
+            Console.WriteLine("Title: " + title); //Display
+            Console.WriteLine();
+
+            Console.ReadKey(true);
+
         }
     }
 }
